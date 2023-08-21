efa903f02c475f93a0ac1db20cc47e88c1670333
marcopete <marco.puga@gmail.com>
2023-08-21 19:14:18 +0000

    Desde local input

--- primerProyecto.py
+++ primerProyecto.py
@@ -1,1 +1,17 @@
-print("Desdelocal")
+print("Me llamo \"Marco\"")
+# Me llamo "Marco"
+print("Esto es una linea\nY esta es otra")
+# Esto es una linea
+# Y esta es otra
+print("\tEsto es una linea tercera con tabulacion")
+    # Esto es una linea tercera con tabulacion
+print('Isn\'t a number')
+# Isn't a number    
+print('Este signo \\ es una barra invertida')
+print("Linea 1\nLinea 2\nLinea 3")
+print("Barra normal: /\nBarra invertida: \\")
+#######################
+# input("Dime tu nombre: ")
+# input("Dime tu apellido: ")
+
+print("Tu nombre es " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
